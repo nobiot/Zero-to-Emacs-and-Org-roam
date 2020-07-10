c1a4ecbe79f6c442f868871d1a759d1d821894e8
Noboru Ota <me@nobiot.com>
2020-07-10 14:29:37 +0200

    Change auto-init to prio MELPA Stable

--- automatic-installation/install-org-roam.el
+++ automatic-installation/install-org-roam.el
@@ -22,6 +22,7 @@
   
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
 
